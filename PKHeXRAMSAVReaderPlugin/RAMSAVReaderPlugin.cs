f53c30f08a4fdc77df3b9ab497559c8ef8d0cd6d
BlackShark <Bl4ckSh4rk@users.noreply.github.com>
2022-06-26 20:02:50 +0200

    Initial commit

---
+++ PKHeXRAMSAVReaderPlugin/RAMSAVReaderPlugin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using PKHeX.Core;
+
+namespace PKHeXRAMSAVReaderPlugin
+{
+    public class RAMSAVReaderPlugin : IPlugin
+    {
+        public string Name => "RAMSAV Reader";
+        public int Priority => 1000;
+
+        public ISaveFileProvider SaveFileEditor => throw new NotImplementedException();
+
+        public void Initialize(params object[] args)
+        {
+            Console.WriteLine($"Loading {Name}...");
+
+            SaveUtil.CustomSaveReaders.Add(new SaveReaderRAMSAV());
+        }
+
+        public void NotifySaveLoaded()
+        {
+            Console.WriteLine($"{Name} was notified that a Save File was just loaded.");
+        }
+
+        public bool TryLoadFile(string filePath)
+        {
+            Console.WriteLine($"{Name} was provided with the file path, but chose to do nothing with it.");
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# PKHeX RAM2SAV Reader Plugin
+[PKHeX](https://github.com/kwsch/PKHeX) plugin to enable reading of old *ramsav.bin* files (Gen 6).
+
+RAM2SAV has been **_deprecated_** since *2016*. *Saving* the *ramsav.bin* is **_not_** supported! Use **_Custom Firmware (CFW) / Homebrew_** to properly export and import *main* save files nowadays.
+
+## How to use
+Put the plugin into the *plugins* folder in your PKHeX directory. Create it if it doesn't exist.
+If PKHeX crashes or you are unable to load your *ramsav.bin*, make sure to unblock the plugin. Left click the .dll, go to Properties and check *Unblock*.
+
+You will then be able to load *ramsav.bin* files like regular save files.
+
+## Credits
+Kaphotics for [PKHeX](https://github.com/kwsch/PKHeX) and the [RAM2SAV logic](https://github.com/kwsch/PKHeX/blob/6ab7ba4bc1f9cb45f3617ce3202e63a0cf9928db/Misc/ram2sav.cs).
